917fb5db7f97c506e501e9709c6884bf14940515
Moo <mohamedaly4122003@gmail.com>
2023-10-14 21:04:25 +0200

    ss

--- index.php
+++ index.php
@@ -10,5 +10,40 @@
 <?php
 include("include/header.php");
 ?>
+<!-- to keep space -->
+<div style="margin-top: 50px;"></div>
+<!-- photos -->
+<div class="container">
+    <div class="col-md-12">
+        <div class="row">
+            <div class="col-md-3 mx-1 shadow">
+            <img src="img/doctor.jpg">
+            <h5 class="text-center">we are employing for doctors</h5>
+            <a href="#">
+                <button class="btn btn-success" style="margin-left:30%;">Apply Now!!</button>
+            </a>
+            </div>
+            <div class="col-md-3 mx-1 shadow" >
+            <img src="img/patient.jpg">
+            <h5 class="text-center">Create an account to know more about you and take care of you</h5>
+            <a href="#">
+                <button class="btn btn-success" style="margin-left:30%;">Create account!!</button>
+            </a>
+            
+            </div>
+
+            <div class="col-md-3 mx-1 shadow">
+            <img src="img/moreinformstion.jpg">
+            <h5 class="text-center">Click here for more information</h5>
+            <a href="#">
+                <button class="btn btn-success" style="margin-left:30%;">MORE</button>
+            </a>
+            </div>
+            
+        </div>
+
+    </div>
+
+</div>
 </body>
 </html>
