4e7d7463eeff22d801f3b41b1d4752d8f464bfe3
Moo <mohamedaly4122003@gmail.com>
2023-10-14 18:31:21 +0200

    ss

---
+++ include/header.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title></title>
+    <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.js" integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous">
+    </script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css">
+</head>
+<body>
+    
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,25 @@
+<?php
+include("include/header.php");
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>HMS home page</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-info bg-info">
+        <h5 class="text-white">Hospital Managment System</h5>
+
+        <div class="mr-auto"></div>
+        <ul class="">
+            <li class="nav-item"><a href="#" class="nav-link">Admin</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Doctor</a></li>
+            <li class="nav-item"><a href="#" class="nav-link">Patient</a></li>
+        </ul>
+
+    </nav>
+</body>
+</html>
