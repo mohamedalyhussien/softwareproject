54dc858a29e7c9508687be4d29b0d259abc9b930
Robert <Robert.raafat90@gmail.com>
2023-10-16 17:04:39 +0300

    AdminLogin

--- include/header.php
+++ include/header.php
@@ -5,10 +5,10 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title></title>
     <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/css/bootstrap.min.css">
-    <!-- <script src="https://code.jquery.com/jquery-3.7.1.slim.js" integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous">
+    <script src="https://code.jquery.com/jquery-3.7.1.slim.js" integrity="sha256-UgvvN8vBkgO0luPSUl2s8TIlOSYRoGFAX4jlCIm9Adc=" crossorigin="anonymous">
     </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
-    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css"> -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-info bg-info">
@@ -16,9 +16,19 @@
 
         <div class="mr-auto" ></div>
         <ul class="navbar-nav">
-            <li class="nav-item"><a href="#" class="nav-link text-white">Admin</a></li>
-            <li class="nav-item"><a href="#" class="nav-link text-white">Doctor</a></li>
-            <li class="nav-item"><a href="#" class="nav-link text-white">Patient</a></li>
+            <?php
+            if(isset($_SESSION['admin'])){
+                $user=$_SESSION['admin'];
+                echo '
+                <li class="nav-item"><a href="#" class="nav-link text-white">'.$user.'</a></li>
+                <li class="nav-item"><a href="logout" class="nav-link text-white">logout</a></li>';
+
+            }else{
+                echo '<li class="nav-item"><a href="adminlogin.php" class="nav-link text-white">Admin</a></li>
+                <li class="nav-item"><a href="#" class="nav-link text-white">Doctor</a></li>
+                <li class="nav-item"><a href="#" class="nav-link text-white">Patient</a></li>';
+            }
+            ?>
         </ul>
 
     </nav>
