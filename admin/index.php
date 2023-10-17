e6e73d9d6bd7ad221daaaa42e6e65f1cb974ef4b
Robert <Robert.raafat90@gmail.com>
2023-10-17 14:34:03 +0300

    Admin Dashboard

--- admin/index.php
+++ admin/index.php
@@ -4,13 +4,127 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
-     <title></title>
+     <title>Admin Dashboard</title>
 </head>
 <body>
     <?php
     
     include("../include/header.php");
-    
+    include("../include/connection.php");
     ?>
+    <div class="container-fluid">
+        <div class="col-md-12">
+            <div class="row">
+                <div class="col-md-2" style="margin-left:-20px">
+
+                <?php
+                include("sidenav.php");
+                ?>
+
+                </div>
+                <div class="col-md-10">
+                    <h4 class="my-2">Admin Dashboard</h4>
+                <div class="col-md-12 my-5">
+                    <div class="row"> 
+                        <div class="col-md-3 bg-success mx-1" style="height:130px;">
+                                <div class ="col-md-12">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <?php
+                                            $ad=mysqli_query($connect,"SELECT * FROM admin ");
+                                            $num=mysqli_num_rows($ad);
+                                            ?>
+                                            <h5 class="my-2 text-white" style="font-size:30px;"><?php echo $num; ?></h5>
+                                            <h5 class="text-white">Total</h5>
+                                            <h5 class="text-white">Admin</h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                           <a href=""> <i class="fa fa-users-cog fa-3x my-2" style="color:white;"></i></a>
+                                    </div>
+                                    </div>
+                                    
+                                </div>
+                        </div>
+                        <div class="col-md-3 bg-info mx-1" style="height:130px;">
+                        <div class ="col-md-12">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h5 class="my-2 text-white" style="font-size:30px;">0</h5>
+                                            <h5 class="text-white">Total</h5>
+                                            <h5 class="text-white">Doctors</h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                           <a href=""> <i class="fa fa-user-md fa-3x my-2" style="color:white;"></i></a>
+                                    </div>
+                                    </div>
+                                    
+                                </div></div>
+                        <div class="col-md-3 bg-warning mx-2" style="height:130px;">
+                        <div class ="col-md-12">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h5 class="my-2 text-white" style="font-size:30px;">0</h5>
+                                            <h5 class="text-white">Total</h5>
+                                            <h5 class="text-white">Patients</h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                           <a href=""> <i class="fa fa-procedures fa-3x my-2" style="color:white;"></i></a>
+                                    </div>
+                                    </div>
+                                    
+                                </div>
+                        </div>
+                        <div class="col-md-3 bg-danger mx-1 my-2" style="height:130px;">
+                        <div class ="col-md-12">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h5 class="my-2 text-white" style="font-size:30px;">0</h5>
+                                            <h5 class="text-white">Total</h5>
+                                            <h5 class="text-white">Reports</h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                           <a href=""> <i class="fa fa-flag fa-3x my-2" style="color:white;"></i></a>
+                                    </div>
+                                    </div>
+                                    
+                                </div>
+                        </div>
+                        <div class="col-md-3 bg-warning mx-1 my-2" style="height:130px;">
+                        <div class ="col-md-12">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h5 class="my-2 text-white" style="font-size:30px;">0</h5>
+                                            <h5 class="text-white">Total</h5>
+                                            <h5 class="text-white">Job Requests</h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                           <a href=""> <i class="fa fa-book-open fa-3x my-2" style="color:white;"></i></a>
+                                    </div>
+                                    </div>
+                                    
+                                </div>
+                        </div>
+                        <div class="col-md-3 bg-success mx-1 my-2" style="height:130px;">
+                        <div class ="col-md-12">
+                                    <div class="row">
+                                        <div class="col-md-9">
+                                            <h5 class="my-2 text-white" style="font-size:30px;">0</h5>
+                                            <h5 class="text-white">Total</h5>
+                                            <h5 class="text-white">Income</h5>
+                                        </div>
+                                        <div class="col-md-3">
+                                           <a href=""> <i class="fa fa-money-check-alt fa-3x my-2" style="color:white;"></i></a>
+                                    </div>
+                                    </div>
+                                    
+                                </div>
+                        </div>
+                    </div>
+                </div>
+
+                </div>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
--- include/header.php
+++ include/header.php
@@ -9,6 +9,8 @@
     </script>
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@fortawesome/fontawesome-free@6.2.1/css/fontawesome.min.css">
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/js/all.min.js"></script>
+    <link rel="stylesheet" type="tex/css" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.2/css/all.min.css">
 </head>
 <body>
     <nav class="navbar navbar-expand-lg navbar-info bg-info">
