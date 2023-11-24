76cbaf43226fdf2139ea28a30c8f5d0d04a56f16
Robert <Robert.raafat90@gmail.com>
2023-11-24 14:31:44 +0200

    sss

--- patient/index.php
+++ patient/index.php
@@ -12,6 +12,7 @@ session_start();
 <body>
     <?php
     include("../include/header.php");
+    include("../include/connection.php");
     ?>
 
     <div class="container-fluid">
@@ -24,6 +25,112 @@ session_start();
                 </div>
                 <div class="col-md-10">
                     <h5 class="my-3">Patient Dashboard</h5>
+                    <div class="col-md-12">
+                        
+                            <div class="row">
+                                <div class="col-md-3 bg-info mx-2" style="height:150px;">
+
+                                <div class="col-md-12">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <h5 class="text-white my-4">My Profile</h5>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <a href="#">
+                                                <i class="fa fa-user-circle fa-3x my-4" style="color:white;"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                </div>
+
+                                <div class="col-md-3 bg-warning mx-2" style="height: 150px;">
+
+                                <div class="col-md-12">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <h5 class="text-white my-4">Book Appointment</h5>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <a href="#">
+                                                <i class="fa fa-calendar fa-3x my-4" style="color:white;"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                </div>
+
+                                <div class="col-md-3 bg-success mx-2" style="height:150px;">
+
+                                <div class="col-md-12">
+                                    <div class="row">
+                                        <div class="col-md-8">
+                                            <h5 class="text-white my-4">My Invoice</h5>
+                                        </div>
+                                        <div class="col-md-4">
+                                            <a href="#">
+                                                <i class="fas fa-file-invoice-dollar fa-3x my-4" style="color:white;"></i>
+                                            </a>
+                                        </div>
+                                    </div>
+                                </div>
+                                
+                                </div>
+                            </div>
+                        
+                    </div>
+
+                    <?php
+                        if(isset($_POST['send'])){
+                            $title=$_POST['send'];
+                            $message=$_POST['message'];
+
+
+                            if(empty($title))
+                            {
+
+                            }else if(empty($message))
+                            {
+
+                            }else{
+                                $user = $_SESSION['patient'];
+                                $query = "INSERT INTO report(title,message,username,date_send) Values('$title','$message','$user',NOW())";
+                                $res= mysqli_query($connect,$query);
+
+                                if($res)
+                                {
+
+                                    echo "<script> alert('You have sent your Report')</script> ";
+                                }
+                            }
+                        }
+                        
+
+
+                    ?>
+
+                    <div class="col-md-12">
+                        <div class="row">
+                            <div class="col-md-3"></div>
+                            <div class="col-md-6 jumbotron bg-info my-5">
+                                <h5 class="text-center my-2">Send A Report</h5>
+                                <form method="post">
+                                    <label> Title</label>
+                                    <input type="text" name="title"autocomplete ="off" class="form-control" placeholder="Enter Title of the report">
+                                    <label>Message</label>
+                                    <input type="text" name="message" autocomplete="off"class="form-control" placeholder="Enter Message">
+                                    <input type="submit" name="send" value="Send Report"class="btn btn-success my-2">
+
+                                </form>
+                            </div>
+                            <div class="col-md-3"></div>
+                        </div>
+                    </div>
+
+
+
                 </div>
             </div>
         </div>
